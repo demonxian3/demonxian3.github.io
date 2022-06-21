58522e686a3b4264afa7f65522c3011d970e8e59
demonxian3 <demonxian3@qq.com>
2022-06-22 07:09:51 +0800

    1

--- sxsh/lib/common.js
+++ sxsh/lib/common.js
@@ -2,14 +2,17 @@ Vue.use({
     install(Vue, options) {
         Vue.mixin({
             data: {
+                order: [],
                 goods: [],
+                finishOrder: [],
+                currentOrder: {},
                 toast: null,
             },
             methods: {
                 loadGoods() {
                     try {
                         this.goods =
-                            JSON.parse(localStorage.getItem('goods')) || []
+                            JSON.parse(localStorage.getItem("goods")) || []
                     } catch (e) {
                         this.goods = []
                     }
@@ -17,16 +20,47 @@ Vue.use({
                     if (!this.goods.length) {
                         this.goods = window.goods
                         localStorage.setItem(
-                            'goods',
+                            "goods",
                             JSON.stringify(this.goods),
                         )
                     }
 
                     this.goods.forEach((good) => {
                         if (!good.sType) {
-                            good.sType = '无分类'
+                            good.sType = "无分类"
                         }
                     })
+                },
+                loadOrder() {
+                    try {
+                        this.order =
+                            JSON.parse(localStorage.getItem("order")) || []
+                    } catch (e) {
+                        this.order = []
+                    }
+
+                    if (!this.order.length) {
+                        localStorage.setItem(
+                            "order",
+                            JSON.stringify(this.order),
+                        )
+                    }
+                },
+                loadCurrentOrder(withMeta = false) {
+                    if ((co = localStorage.getItem("currentOrder"))) {
+                        if (!withMeta) {
+                            this.currentOrder = JSON.parse(co).goods
+                        } else {
+                            this.currentOrder = JSON.parse(co)
+                        }
+                    }
+                },
+                loadFinishOrder(){
+                    try {
+                        this.finishOrder = JSON.parse(localStorage.getItem("finishOrder")) || []
+                    } catch (e) {
+                        this.finishOrder = []
+                    }
                 },
 
                 getTypeList(goods = null) {
@@ -39,7 +73,7 @@ Vue.use({
                         if (good.sType) {
                             typeMap[good.sType] = 1
                         } else {
-                            good.sType = '无分类'
+                            good.sType = "无分类"
                         }
                     })
 
@@ -53,17 +87,17 @@ Vue.use({
                 },
             },
             mounted() {
-                if (typeof Swal !== 'undefined') {
+                if (typeof Swal !== "undefined") {
                     const swalInstance = Swal.mixin({
                         toast: true,
-                        position: 'top',
+                        position: "top",
                         showConfirmButton: false,
                         timer: 1200,
                         timerProgressBar: false,
                         didOpen: (toast) => {
-                            toast.addEventListener('mouseenter', Swal.stopTimer)
+                            toast.addEventListener("mouseenter", Swal.stopTimer)
                             toast.addEventListener(
-                                'mouseleave',
+                                "mouseleave",
                                 Swal.resumeTimer,
                             )
                         },
@@ -71,19 +105,19 @@ Vue.use({
 
                     this.toast = {
                         success: (title) => {
-                            swalInstance.fire({ title, icon: 'success' })
+                            swalInstance.fire({ title, icon: "success" })
                         },
                         error: (title) => {
-                            swalInstance.fire({ title, icon: 'error' })
+                            swalInstance.fire({ title, icon: "error" })
                         },
                         warning: (title) => {
-                            swalInstance.fire({ title, icon: 'warning' })
+                            swalInstance.fire({ title, icon: "warning" })
                         },
                         info: (title) => {
-                            swalInstance.fire({ title, icon: 'info' })
+                            swalInstance.fire({ title, icon: "info" })
                         },
                         question: (title) => {
-                            swalInstance.fire({ title, icon: 'question' })
+                            swalInstance.fire({ title, icon: "question" })
                         },
                         confirm: async (title) => {
                             return swalInstance.fire({
@@ -91,9 +125,9 @@ Vue.use({
                                 timer: 3500,
                                 showCancelButton: true,
                                 showConfirmButton: true,
-                                confirmButtonText: '恢复',
-                                cancelButtonText: '取消',
-                                icon: 'question',
+                                confirmButtonText: "恢复",
+                                cancelButtonText: "取消",
+                                icon: "question",
                             })
                         },
                     }
@@ -104,13 +138,16 @@ Vue.use({
 })
 
 $nav = $(`<nav class="navbar navbar-dark bg-info">
-<a class="navbar-brand" href="index.html">盛兴商店</a>
+<a class="navbar-brand" href="cashier.html">盛兴商店</a>
 <ul class="navbar-nav mr-auto d-flex" style="flex-direction: row">
     <li class="nav-item mr-3" >
         <a class="nav-link pb-1" href="cashier.html">收银</a>
     </li>
     <li class="nav-item mr-3" >
-        <a class="nav-link pb-1" href="index.html">查询</a>
+        <a class="nav-link pb-1" href="order.html">订单</a>
+    </li>
+    <li class="nav-item mr-3" >
+        <a class="nav-link pb-1" href="goods.html">商品</a>
     </li>
     <li class="nav-item mr-3">
         <a class="nav-link pb-1" href="create.html">添加</a>
